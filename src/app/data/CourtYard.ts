1cb224ab1180345b710810d0232f5565f51c1f95
friederndevSoE <friedern.dev@outlook.com>
2025-09-23 13:18:22 +0700

    Update CourtYard.ts

--- src/app/data/CourtYard.ts
+++ src/app/data/CourtYard.ts
@@ -1,27 +1,53 @@
 export const englishSections = [
   {
-    text: `"ONEEE why me"?
-some deserts on this planet, were oceans once
-somewhere shrouded by the night, the sun will shine
-sometimes i see dying birds, fall to the ground
-but it used to fly so high`,
-    style: "quote",
-  },
-  { text: "---", style: "hr" },
-  {
-    text: `too much of the past, for one to memorize
-too many words remained, for one to read through the lines
-the ebb and flow of the crowd, floods the world, and paradise
-along the path of time`,
+    text: `To be able to play and enjoy gaming is a privilege. After all, this is just a bubble, here in this space, you can feel the pure human souls expressed through the stories told in this medium.
+`,
     style: "default",
   },
+  //line break
+  { text: "---", style: "hr" },
   {
-    text: `say my name, when the trees, susurrate
-once and again, tell a story lost in time
-the way it starts, and the way it ends
-never again, leaving a story in dismay
-with several starts, but just one ends.`,
-    style: "conversation",
+    text: `Visitor, welcome to the courtyard. This is the stage, the origin of everything here.
+
+I approach gaming as an experience no different than reading a story or watching a movie. I want to focus on the story and the creative ways it is presented through this interactive art form.
+
+A story where you can feel the political oppression and propaganda at every corner, even though humanity has evolved to the point where we can travel the stars? Eerily real and captivating that it sticks with you and makes you glance at how real life works.
+A story that explores a much more niche and personal problem, like trauma, and makes you confront guilt, making game decisions that make you feel uneasy in your stomach? Finishing it makes you question the hardship other unfortunate souls have to get through daily.
+A story that instantly takes you back to the fond memory of your school days, reminds you of the friends and genuine connections you once made, with every interaction makes you feel the warmth inside, and you can’t help but feel deep appreciation and want to tell all your friends about.
+
+In a way, it’s the same as every other story you heard, and yet it’s not really so.
+
+Here, you are the person who makes the decision, chooses, and interacts on your own. You reap the reward based on your action, reach the light at the end of the tunnel, but also bear the consequences should things go south.
+
+Your choice and interaction make every feeling stronger than ever.
+
+After a very ‘short’ time of back and forth thinking, I called this one StardomEphemeraArchivelMomentaryProjectArcadiaEchoRepertoire: Eden Courtyard (name subject to change) [25.5.21]
+`,
+    style: "default",
+  },
+
+  //line break
+  { text: "---", style: "hr" },
+  {
+    text: `I find joy knowing that across different lives, experiences, or wisdom, other souls, too, share the passion and authentic expression, pouring themselves into the story they want to tell. They manage to capture the heartfelt moment and tell a genuine tale, wrap it in a creative format called a game that can reach and inspire so many people.
+
+This place, too, is built with the same desire, but I also aim for something more romantic, to preserve emotion and memories.
+
+Maybe one day, the reason behind this project might no longer be true, the impact is no longer aligned with what it was intended, things will change, as everything always is… perhaps in a way I might not look forward to.
+
+I wonder the day when its meaning is lost, does the flame engulf, or just slowly dissipates with time?
+
+The courtyard should stay as it is.
+
+If that day comes, I want to hear your “response” too. Is this the curtain call that I wish?
+.`,
+    style: "default",
+  },
+  //line break
+  { text: "---", style: "hr" },
+  {
+    text: `Dear visitor, I hope you enjoy your stay and hopefully find something that resonates or sparks your interest here. Take what you will in this moment, if something manages to stay with you faintly, then the light has fulfilled its purpose..`,
+    style: "default",
   },
   { text: "---", style: "hr" },
 ];
